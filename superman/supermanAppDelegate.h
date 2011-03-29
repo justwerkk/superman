d89a1986302eb5428ea7928165d6956b1662ba26
Steven Huang <stevenhuang@Steven-Huangs-MacBook-Pro.local>
2011-03-28 22:57:40 -0700

    Initial Commit

---
+++ superman/supermanAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  supermanAppDelegate.h
+//  superman
+//
+//  Created by Steven Huang on 3/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface supermanAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
